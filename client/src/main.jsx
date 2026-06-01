fe1a00e5517dd83e9b4b9391c77b74f72b65a1d1
Thamu <movieinsight908@gmail.com>
2026-06-01 21:38:06 +0530

    fix: use createBrowserRouter for ScrollRestoration compat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
+const router = createBrowserRouter([
+  { path: '*', element: <App /> },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </StrictMode>,
 )
