b5a5cf697124dbb5f7655ec53deadccdd335b1ee
Thamu <movieinsight908@gmail.com>
2026-06-04 20:53:35 +0530

    remove unused OpenAI trivia import, keep TMDB fixes

--- server/generateTrivia.js
+++ server/generateTrivia.js
@@ -1,6 +1,6 @@
 import { buildTmdbUrl, getTmdbHeaders } from './db.js';
 import { createCineUpdate } from './db.js';
-import { generateLocalizedTrivia } from './openai.js';
+
 
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 
@@ -294,13 +294,7 @@ export async function generateTriviaUpdates(count = 20) {
     const template = getRandomTemplate(details);
     if (!template) continue;
 
-    let trivia;
-    if (['ta', 'ml'].includes(details.original_language)) {
-      const localized = await generateLocalizedTrivia(details, template.category, details.original_language);
-      trivia = localized || template.generate(details);
-    } else {
-      trivia = template.generate(details);
-    }
+    const trivia = template.generate(details);
     updates.push(movieToUpdate(details, trivia, template.category));
   }
 
