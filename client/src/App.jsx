5cbd9b6b438bf9ed887d798a3fb8a7841f57f74d
Thamu <movieinsight908@gmail.com>
2026-06-04 22:21:13 +0530

    feat: replace navbar text logo with Cloudinary image logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1247,7 +1247,7 @@ const handleDeleteReview = useCallback(async (reviewId) => {
       <div className="navbar-container">
         <nav className="navbar">
           <div className="logo" onClick={() => { navigateTo('home'); }}>
-            Thirai<span>Pedia</span>
+            <img src="https://res.cloudinary.com/di0j4psxz/image/upload/v1780591749/New_Project_3_cn7had.png" alt="ThiraiPedia" className="logo-img" />
           </div>
           <div className={`nav-links ${isMobileMenuOpen ? 'nav-links--open' : ''}`}>
             <Link 
